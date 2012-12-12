1772a597b791da1da0bcee474db55ef2e5f101d9
Hameed <machdd@MacHDDs-MacBook-Pro.local>
2012-12-12 23:21:15 +0300

    Added new Control

---
+++ test1/test1/New Control/testingObjects.h
@@ -0,0 +1,13 @@
+//
+//  testingObjects.h
+//  test1
+//
+//  Created by Hameed on 12/12/12.
+//  Copyright (c) 2012 Hameed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testingObjects : NSObject
+
+@end
---
+++ test1/test1/New Control/testingObjects.m
@@ -0,0 +1,13 @@
+//
+//  testingObjects.m
+//  test1
+//
+//  Created by Hameed on 12/12/12.
+//  Copyright (c) 2012 Hameed. All rights reserved.
+//
+
+#import "testingObjects.h"
+
+@implementation testingObjects
+
+@end
