8d6d48340d5e6a05b2d7beeb3667960a6ea3c764
Hameed <machdd@MacHDDs-MacBook-Pro.local>
2012-12-12 23:12:12 +0300

    first

---
+++ test1/test1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by Hameed on 12/12/12.
+//  Copyright (c) 2012 Hameed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ test1/test1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test1
+//
+//  Created by Hameed on 12/12/12.
+//  Copyright (c) 2012 Hameed. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
