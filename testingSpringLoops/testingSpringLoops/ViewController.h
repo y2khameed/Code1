501c6a4ed218fcefc2e8cce51ffc9ac9561ddb20
Hameed <machdd@MacHDDs-MacBook-Pro.local>
2012-12-14 21:44:59 +0300

    frist

---
+++ testingSpringLoops/testingSpringLoops/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testingSpringLoops
+//
+//  Created by Hameed on 12/14/12.
+//  Copyright (c) 2012 Hameed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ testingSpringLoops/testingSpringLoops/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testingSpringLoops
+//
+//  Created by Hameed on 12/14/12.
+//  Copyright (c) 2012 Hameed. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
